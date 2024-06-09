1ea46aa2e77d445b40b18e88d5b23c11d256d0db
ded2322 <151318767+ded2322@users.noreply.github.com>
2024-06-09 17:43:47 +0700

    v.1.0

--- core/routers/router.py
+++ core/routers/router.py
@@ -3,12 +3,12 @@ from fastapi import APIRouter
 from core.dao.dao import BaseDao
 
 router = APIRouter(
-    prefix="/show-video-service",
-    tags=["Show-video-service"],
+    prefix="/streaming-microservice",
+    tags=["Streaming microservice"],
 )
 
 
-@router.get("/last-videos", status_code=200, summary="Show all videos")
+@router.get("/last-videos", status_code=200, summary="Loading last 50 videos")
 async def show_video_service():
     """
     Отображает последние 50 видео
@@ -16,7 +16,7 @@ async def show_video_service():
     return await BaseDao.show_50_video()
 
 
-@router.get("/load")
+@router.get("/loading-videos", status_code=200, summary="Loading loading additional 50 messages")
 async def load_video(video_id: int):
     """
     Подгружает дополнительные 50 видео
